8c7438f0b8fc45a8f7e89cdce80dca354e5b4e71
Yaroslav Konoplov <eahome00@gmail.com>
2018-01-10 02:59:41 +0200

    Remove test files related to Test::Unit.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Barong
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Stop Rails generator from creating unneeded scripts.
+    config.generators.assets = false
+    config.generators.helper = false
+    config.generators.tests  = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
