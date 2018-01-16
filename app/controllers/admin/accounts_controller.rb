e2134a8df2b30c676a8e0c1309ee2f95cd4d82b6
Galya Fedorenko <gfedorenko@heliostech.fr>
2018-01-16 17:52:43 +0200

    Added listing of accounts with ability to delete or edit

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -2,7 +2,33 @@
 
 module Admin
   class AccountsController < ModuleController
+
     def index
+      @accounts = Account.all.page params[:page]
     end
+
+    def destroy
+      Account.find(params[:id]).destroy!
+      respond_to do |format|
+        format.html { redirect_to admin_accounts_url, notice: 'Successfully destroyed.' }
+      end
+    end
+
+    def edit
+      @account = Account.find(params[:id])
+    end
+
+    def update
+      @account = Account.find(params[:id])
+      @account.update_attributes(account_params) if params[:account]
+      redirect_to admin_accounts_path
+    end
+
+  private
+
+    def account_params
+      params.require(:account).permit(:role)
+    end
+
   end
 end
