9e90ab5d2fb2c8d247981c2d42790037b36718d1
Galya Fedorenko <f.g.i.mind@gmail.com>
2018-02-01 22:36:10 +0200

    Return if phone is already in db (#91)

--- app/assets/javascripts/web.js
+++ app/assets/javascripts/web.js
@@ -6,19 +6,20 @@
 
 function sendVerificationCode() {
   number = '+' + $("#country_code").val() + $("#number").val();
-   $.ajax({
-     headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
-     method:  'POST',
-     data:    { number: number },
-     url:     'verification',
-     success: function(result){
-        if (result.success){
-          $("#error").text('');
-          $("#create-phone").prop('disabled', false);
-          $("#send-code-btn").fadeTo( 1000, 0 );
-          $("#send-code-btn").prop('disabled', true);
-        } else {
-          $("#error").text('Phone number is invalid');
-        };
-    }});
+  $.ajax({
+    headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
+    method:  'POST',
+    data:    { number: number },
+    url:     'verification',
+    success: function(result){
+       if (result.success){
+         $("#error").text('');
+         $("#create-phone").prop('disabled', false);
+         $("#send-code-btn").fadeTo( 1000, 0 );
+         $("#send-code-btn").prop('disabled', true);
+       } else {
+         $("#error").text(result.error);
+       }
+    }
+  });
 };
