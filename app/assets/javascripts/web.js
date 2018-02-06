1200b67208f47a99ae06d8ad187364a9ddcfdf01
Galya Fedorenko <f.g.i.mind@gmail.com>
2018-02-06 15:16:01 +0200

    Fix for js (#98)

--- app/assets/javascripts/web.js
+++ app/assets/javascripts/web.js
@@ -4,22 +4,26 @@
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
 
-function sendVerificationCode() {
-  number = '+' + $("#country_code").val() + $("#number").val();
-  $.ajax({
-    headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
-    method:  'POST',
-    data:    { number: number },
-    url:     'verification',
-    success: function(result){
-       if (result.success){
-         $("#error").text('');
-         $("#create-phone").prop('disabled', false);
-         $("#send-code-btn").fadeTo( 1000, 0 );
-         $("#send-code-btn").prop('disabled', true);
-       } else {
-         $("#error").text(result.error);
-       }
-    }
+window.onload = function () {
+  $('.datepicker').datepicker();
+
+  $('#send-code-btn').on('click', function () {
+    number = '+' + $("#country_code").val() + $("#number").val();
+    $.ajax({
+      headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
+      method:  'POST',
+      data:    { number: number },
+      url:     'verification',
+      success: function(result){
+         if (result.success){
+           $("#error").text('');
+           $("#create-phone").prop('disabled', false);
+           $("#send-code-btn").fadeTo( 1000, 0 );
+           $("#send-code-btn").prop('disabled', true);
+         } else {
+           $("#error").text(result.error);
+         }
+      }
+    });
   });
 };
