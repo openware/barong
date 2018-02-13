7c33e56965943d56103b31d9bc3dc9db11a6e056
Ali Shanaakh <ashanaakh@gmail.com>
2018-02-13 13:00:11 +0200

    Use service account for fog [WIP]

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,9 @@
 if Rails.application.secrets.google_storage_bucket
   CarrierWave.configure do |config|
     config.fog_credentials = {
-
-      :provider                         => 'Google',
-      :google_storage_access_key_id     => Rails.application.secrets.google_storage_access_key_id,
-      :google_storage_secret_access_key => Rails.application.secrets.google_storage_secret_access_key
-
-      }
+      provider: 'Google',
+      google_json_key_string: Rails.application.secrets.google_json_key_string
+    }
 
     config.fog_directory = Rails.application.secrets.google_storage_bucket
   end
