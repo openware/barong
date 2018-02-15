a02533550541ffcb0216fa97336c4a1f383c6658
Galya Fedorenko <gfedorenko@heliostech.fr>
2018-02-16 00:34:14 +0200

    Fix for login

---
+++ db/migrate/20180215222851_remove_token_index_limit.rb
@@ -0,0 +1,6 @@
+class RemoveTokenIndexLimit < ActiveRecord::Migration[5.1]
+  def change
+    remove_index  :oauth_access_tokens, :token if index_exists?(:oauth_access_tokens, :token)
+    change_column :oauth_access_tokens, :token, :string, null: false, index: { unique: true }, limit: 1024
+  end
+end
