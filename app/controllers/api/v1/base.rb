7e7eef544de85614ce109f38f6a1f146f0e4739a
Valera Petrusenko <villi1511@gmail.com>
2018-02-20 19:33:14 +0200

    generate API documentation

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -3,7 +3,24 @@
 module API
   module V1
     class Base < Grape::API
+      format :json
+
       mount API::V1::Accounts
+
+      add_swagger_documentation base_path: '/api',
+                                info: {
+                                  title: 'Barong',
+                                  description: 'API for barong OAuth server '
+                                },
+                                api_version: 'v1',
+                                target_class: API::V1::Accounts,
+                                hide_format: true,
+                                hide_documentation_path: true,
+                                mount_path: '/swagger_doc'
+
+      route :any, '*path' do
+        raise StandardError, 'Unable to find endpoint'
+      end
     end
   end
 end
