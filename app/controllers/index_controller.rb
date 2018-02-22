6914a460b3017aa64efef953ffc73c93908b3b60
Galya Fedorenko <f.g.i.mind@gmail.com>
2018-02-22 23:06:47 +0200

    Small fixes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,5 +3,9 @@
 class IndexController < ApplicationController
   def index
     redirect_to new_account_session_url unless account_signed_in?
+    if current_account
+      redirect_to new_phone_path if current_account.level == 1
+      redirect_to new_profile_path if current_account.level == 2 && !current_account.profile.documents
+    end
   end
 end
