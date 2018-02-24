04a080ae85bbd5978825745202af193f93b57f00
Amir Budaychiev <amir.budaychiev@gmail.com>
2018-02-24 05:02:54 +0300

    fixed action mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method      = :smtp
   config.action_mailer.smtp_settings        = { address: 'localhost', port: 1025 }
   config.action_mailer.default_url_options  = { host: 'localhost', port: 3000 }
-  config.action_mailer.asset_host           = { host: 'localhost', port: 3000 }
+  config.action_mailer.asset_host           = 'http://localhost:3000'
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
