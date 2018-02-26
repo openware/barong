874e303f5bd3c353b74e9f99ecf744b799826d2b
Dmytro V <staff1489@gmail.com>
2018-02-26 10:11:56 +0200

    add swagger-ui via yarn

---
+++ app/controllers/swagger_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class SwaggerController < ApplicationController
+  layout 'swagger'
+
+  def index
+  end
+
+end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w[ web.js web.css admin.js admin.css ]
+Rails.application.config.assets.precompile += %w[
+  web.js web.css
+  admin.js admin.css
+  swagger_ui.js swagger_ui.css swagger_ui_print.css swagger_ui_screen.css
+]
