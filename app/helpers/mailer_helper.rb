b40fb3bb57bf3870578de333b5fa2ce1890c60b9
Amir Budaychiev <amir.budaychiev@gmail.com>
2018-02-27 14:26:08 +0300

    fixed logo, added new texts to the emails

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -2,8 +2,6 @@
 
 module MailerHelper
 
-  include ApplicationHelper
-
   def determine_logo_url
     host      = confirmation_url(Account.first) # TODO: need way to determine a host correctly, just tip to me, but it works!
     websites  = Website.all
@@ -11,7 +9,12 @@ module MailerHelper
     websites.each do |website|
       logo_url = website.logo if host.include?(website.domain)
     end
-    domain_logo_tag(logo_url)
+
+    if logo_url.blank?
+      image_tag('logo-black.png')
+    else
+      image_tag(logo_url)
+    end
   end
 
 end
