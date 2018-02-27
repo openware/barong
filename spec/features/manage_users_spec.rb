4b602d8fe463771c1e4cb173f7a5c286428f75b5
Galya Fedorenko <gfedorenko@heliostech.fr>
2018-02-27 14:49:08 +0200

    Fixed tests

--- spec/features/manage_users_spec.rb
+++ spec/features/manage_users_spec.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 describe 'Admin can' do
+  before(:example) do
+    account.update(role: :admin)
+    sign_in account
+    visit admin_accounts_path
+  end
   let(:member) { create :account }
   let(:account) { create :account }
 
   it 'edit roles' do
-    account.update(role: :admin)
-    visit index_path
-    fill_in 'account_email', with: account.email
-    fill_in 'account_password', with: account.password
-    click_on 'Submit'
-    visit admin_accounts_path
     expect(page).to have_content("#{account.email}")
-
     click_link 'Edit'
     select 'admin', from: 'account_role'
     click_on 'Submit'
@@ -20,13 +18,6 @@ describe 'Admin can' do
   end
 
   it 'delete accounts' do
-    account.update(role: :admin)
-    visit index_path
-    fill_in 'account_email', with: account.email
-    fill_in 'account_password', with: account.password
-    click_on 'Submit'
-    visit admin_accounts_path
-
     click_link 'Delete'
     within('div.modal') do
       click_button 'Confirm'
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,12 +2,12 @@
 
 describe 'Sign up' do
   it 'allows to sign up with email, password and password confirmation' do
-    skip
-    visit index_path
+    visit new_account_registration_path
     fill_in 'account_email', with: 'account@accounts.peatio.tech'
     fill_in 'account_password', with: 'B@rong2018'
     fill_in 'account_password_confirmation', with: 'B@rong2018'
     click_on 'Submit'
+
     expect(page).to have_content(/follow the link to activate your account/)
   end
 end
