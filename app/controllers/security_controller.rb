96f732a2f74f44e906e50b3de264c64f13724fc0
Galya Fedorenko <gfedorenko@heliostech.fr>
2018-03-21 20:02:15 +0200

    Added 2fa confirmation with otp

--- app/controllers/security_controller.rb
+++ app/controllers/security_controller.rb
@@ -17,4 +17,36 @@ class SecurityController < ApplicationController
     @otp_secret = CGI.parse(URI.parse(@otp.data[:url]).query)['secret'][0]
   end
 
+  def confirm
+    return redirect_to index_path unless otp_enabled?
+
+    render action: :confirm
+  end
+
+  def validate_otp
+    if Vault::TOTP.validate?(current_account.uid, params[:otp])
+      redirect_to index_path
+    else
+      Vault.logical.delete("totp/keys/#{current_account.uid}")
+      render  security_confirm_path, alert: 'err'
+    end
+  end
+
+private
+
+  def otp_enabled?
+    uid = current_account.uid
+    return false unless uid.present?
+    Vault::TOTP.exist?(uid)
+  end
+
+  def otp_verify
+    return if Vault::TOTP.validate?(find_uid_by_params_email, params[:otp])
+
+    set_flash_message! :alert, :wrong_otp_code
+    redirect_to accounts_sign_in_confirm_path
+  rescue Vault::HTTPClientError => e
+    redirect_to new_account_session_path, alert: "Vault error: #{e.errors.join}"
+  end
+
 end
