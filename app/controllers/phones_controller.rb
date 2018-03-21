9c4e29b6a2e97696264088ba414a7fa68f905d6c
Galya Fedorenko <f.g.i.mind@gmail.com>
2018-03-21 10:15:30 +0200

    Added phone validation tests

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,21 +1,7 @@
 # frozen_string_literal: true
 
-module PhoneUtils
-  class << self
-    def sanitize(unsafe_phone)
-      unsafe_phone.to_s.gsub(/\D/, '')
-    end
-
-    # Phone MUST contain international country code.
-    def valid?(unsafe_phone)
-      number = sanitize(unsafe_phone)
-      phone  = Phonelib.parse(number)
-      phone.valid? && phone.international(false) == number
-    end
-  end
-end
-
 class PhonesController < ApplicationController
+  include PhoneUtils
   before_action :authenticate_account!
 
   def new
