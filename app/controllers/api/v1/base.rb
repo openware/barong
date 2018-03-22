452b6bd828a38cb1382de4e31f29240d6c79d3a5
andreymakovenko <makovenkoandrey@gmail.com>
2018-03-22 16:26:55 +0200

    Implemented API endpoind for receiving JWT by account and password

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -8,6 +8,7 @@ module API
       mount API::V1::Accounts
       mount API::V1::Profiles
       mount API::V1::Security
+      mount API::V1::Session
 
       add_swagger_documentation base_path: '/api',
                                 info: {
---
+++ spec/api/session_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe 'Session create test' do
+  describe 'POST /api/session/create' do
+    let!(:email) { 'user@barong.io' }
+    let!(:password) { 'testpassword111' }
+    let(:uri) { '/api/session/create' }
+    let(:check_uri) { '/api/security/renew' }
+    let!(:application) { create :doorkeeper_application }
+    subject!(:acc) do
+      create :account,
+      email: email,
+      password: password,
+      password_confirmation: password
+    end
+
+    it 'Checks if current credentials are valid and returns valid JWT' do
+      post uri, params: { email: email, password: password, application_id: application.uid }
+      expect(response.status).to eq(201)
+
+      post check_uri,
+           headers: { Authorization: "Bearer #{JSON.parse(response.body)}" }
+      expect(response.status).to eq(201)
+    end
+
+    it 'Checks if current credentials are valid and returns error, cause they are not' do
+      post uri
+      expect(response.status).to eq(401)
+      expect(response.body).to eq("{\"error\":\"401 Unauthorized\"}")
+
+      post uri, params: { email: 'rick@morty.io', password: 'season1' }
+      expect(response.status).to eq(401)
+      expect(response.body).to eq("{\"error\":\"401 Unauthorized\"}")
+
+      post uri, params: { email: email }
+      expect(response.status).to eq(401)
+      expect(response.body).to eq("{\"error\":\"401 Unauthorized Application\"}")
+
+      post uri, params: { email: email, password: password}
+      expect(response.status).to eq(401)
+      expect(response.body).to eq("{\"error\":\"401 Unauthorized Application\"}")
+    end
+  end
+end
