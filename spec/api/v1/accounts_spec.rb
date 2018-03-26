823a6849c96c9149557f9489fa693ddf42568821
andreymakovenko <makovenkoandrey@gmail.com>
2018-03-26 16:10:28 +0300

    Implemented ability to change password by API

--- spec/api/v1/accounts_spec.rb
+++ spec/api/v1/accounts_spec.rb
@@ -64,4 +64,63 @@ describe 'Api::V1::Accounts' do
       end
     end
   end
+  describe 'PUT /api/account/password' do
+    let(:url) { '/api/account/password' }
+    let!(:password0) { 'testpassword111' }
+    let!(:password1) { 'testpassword123' }
+    let(:params0) do
+      {
+        old_password: password0,
+        new_password: password1
+      }
+    end
+
+    let(:params1) do
+      {
+        old_password: password1,
+        new_password: password0
+      }
+    end
+
+    subject!(:acc) do
+      create :account,
+      password: password0,
+      password_confirmation: password0
+    end
+
+    let!(:access_token) do
+      create :doorkeeper_token,
+      resource_owner_id: acc.id
+    end
+
+    let(:headers) do
+      { Authorization: "Bearer #{access_token.token}" }
+    end
+
+    it 'Checks if provided credentials are valid and changes password to the new one' do
+      put url, params: params0, headers: headers
+      expect(response.status).to eq(200)
+
+      put url, params: params1, headers: headers
+      expect(response.status).to eq(200)
+    end
+
+    it 'Checks if credentials are valid and returns error, cause they are not' do
+      put url, params: params1, headers: headers
+      expect(response.status).to eq(401)
+      expect(response.body).to eq('{"error":"401 Unauthorized"}')
+
+      put url
+      expect(response.status).to eq(401)
+      expect(response.body).to eq('{"error":"The access token is invalid"}')
+
+      put url, params: params0.except(:new_password), headers: headers
+      expect(response.status).to eq(400)
+      expect(response.body).to eq('{"error":"new_password is missing"}')
+
+      put url, params: params0.except(:old_password), headers: headers
+      expect(response.status).to eq(400)
+      expect(response.body).to eq('{"error":"old_password is missing"}')
+    end
+  end
 end
