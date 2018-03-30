7c96049744d00ff56d2c5024d76335004f0872cd
Maksim Litvinov <maksim-litvinov@users.noreply.github.com>
2018-03-30 14:23:25 +0300

    Move account registration to account endpoint

--- app/controllers/api/v1/accounts.rb
+++ app/controllers/api/v1/accounts.rb
@@ -7,29 +7,41 @@ module API
     class Accounts < Grape::API
       helpers Doorkeeper::Grape::Helpers
 
-      before do
-        doorkeeper_authorize!
-
-        def current_account
-          @current_account = Account.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-        end
-      end
-
       desc 'Account related routes'
       resource :account do
-        desc 'Return information about current resource owner'
-        get '/' do
-          current_account.as_json(only: %i[uid email level role state])
+        desc 'Creates new account'
+        params do
+          requires :email, type: String, desc: 'Account Email'
+          requires :password, type: String, desc: 'Account Password'
+        end
+        post do
+          account = Account.create(email: params[:email], password: params[:password])
+          error!(account.errors.full_messages, 422) unless account.persisted?
         end
 
-        desc 'Confirms an account'
-        params do
-          requires :confirmation_token, type: String, desc: 'Confirmation token from email'
-        end
-        patch '/confirm' do
-          account = Account.find_first_by_auth_conditions confirmation_token: params[:confirmation_token]
-          return error!('Confirmation token is invalid', 422) if account != current_account
-          error!('Account is already confirmed', 422) unless current_account.confirm
+        namespace do
+          before do
+            doorkeeper_authorize!
+
+            def current_account
+              @current_account = Account.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+            end
+          end
+
+          desc 'Return information about current resource owner'
+          get '/' do
+            current_account.as_json(only: %i[uid email level role state])
+          end
+
+          desc 'Confirms an account'
+          params do
+            requires :confirmation_token, type: String, desc: 'Confirmation token from email'
+          end
+          patch '/confirm' do
+            account = Account.find_first_by_auth_conditions confirmation_token: params[:confirmation_token]
+            return error!('Confirmation token is invalid', 422) if account != current_account
+            error!('Account is already confirmed', 422) unless current_account.confirm
+          end
         end
       end
     end
--- spec/api/v1/accounts_spec.rb
+++ spec/api/v1/accounts_spec.rb
@@ -22,6 +22,49 @@ describe 'Api::V1::Accounts' do
     end
   end
 
+  describe 'POST /api/account' do
+    let(:do_request) do
+      post '/api/account', params: params
+    end
+
+    before { do_request }
+
+    context 'when email is invalid' do
+      let(:params) { { email: 'bad_format', password: 'password' } }
+
+      it 'renders an error' do
+        expect_status_to_eq 422
+        expect_body.to eq(error: ['Email is invalid'])
+      end
+    end
+
+    context 'when email is blank' do
+      let(:params) { { email: '', password: 'password' } }
+
+      it 'renders an error' do
+        expect_status_to_eq 422
+        expect_body.to eq(error: ["Email can't be blank"])
+      end
+    end
+
+    context 'when email and password are absent' do
+      let(:params) {}
+
+      it 'renders an error' do
+        expect_status_to_eq 400
+        expect_body.to eq(error: 'email is missing, password is missing')
+      end
+    end
+
+    context 'when email is valid' do
+      let(:params) { { email: Faker::Internet.email, password: 'password' } }
+
+      it 'creates an account' do
+        expect_status_to_eq 201
+      end
+    end
+  end
+
   describe 'PATCH /api/account/confirm' do
     let(:do_request) do
       patch '/api/account/confirm', params: params, headers: auth_header
