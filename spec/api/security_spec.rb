0b3c4fe7304a3d786ec59779085ca9ccd3c16415
andreymakovenko <makovenkoandrey@gmail.com>
2018-04-05 21:10:56 +0300

    Add '/v1' prefix for API::V1 routes

--- spec/api/security_spec.rb
+++ spec/api/security_spec.rb
@@ -6,34 +6,35 @@ describe 'JWT renewal test' do
     let(:headers) do
       { Authorization: "Bearer #{access_token.token}" }
     end
+    let(:url) { '/api/v1/security/renew' }
 
     it 'Checks if current JWT is valid and returns new valid JWT' do
-      post '/api/security/renew', headers: headers
+      post url, headers: headers
       expect(response.status).to eq(201)
 
-      post '/api/security/renew',
+      post url,
            headers: { Authorization: "Bearer #{JSON.parse(response.body)}" }
       expect(response.status).to eq(201)
     end
 
     it 'Checks if current JWT is valid and returns JWT with specified liftime' do
-      post '/api/security/renew', params: { expires_in: 1000 }, headers: headers
+      post url, params: { expires_in: 1000 }, headers: headers
       expect(response.status).to eq(201)
       new_jwt_payload = JWT.decode JSON.parse(response.body), nil, false
       expect(new_jwt_payload.first['exp'].to_i).to be <= Time.now.to_i + 1000
     end
 
     it 'Checks if current JWT is valid and returns error, cause it is not' do
-      post '/api/security/renew'
+      post url
       expect(response.body).to eq("{\"error\":\"The access token is invalid\"}")
     end
 
     it 'Checks if current JWT is valid and returns error, cause it has expired' do
-      post '/api/security/renew', params: { expires_in: 1 }, headers: headers
+      post url, params: { expires_in: 1 }, headers: headers
       expect(response.status).to eq(201)
       new_jwt = JSON.parse(response.body)
       sleep(2)
-      post '/api/security/renew', headers: { 'Authorization' => "Bearer #{new_jwt}"}
+      post url, headers: { 'Authorization' => "Bearer #{new_jwt}"}
       expect(response.body).to eq("{\"error\":\"The access token expired\"}")
     end
   end
