7f1e7c81744015ca0568433d8a668499b28c0aa9
Maksim Litvinov <maksim-litvinov@users.noreply.github.com>
2018-04-10 19:39:49 +0300

    Fix phone verification issue

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,40 +1,31 @@
 # frozen_string_literal: true
 
 class PhonesController < ApplicationController
-  include PhoneUtils
   before_action :authenticate_account!
+  before_action :check_account_level
+  before_action :check_phone, only: :create
+  before_action :check_code, only: :create
+
+  rescue_from Twilio::REST::RestError, with: :twilio_errors
 
   def new
-    if current_account.level < 1
-      redirect_to index_path
-    else
-      @phone = Phone.new
-    end
+    @phone = Phone.new
   end
 
   def create
-    number = PhoneUtils.sanitize(phone_params[:country_code] + phone_params[:number])
-    unless session[:phone] == number
-      return redirect_to new_phone_url, notice: 'Confirmation code was sent to another number'
-    end
-
-    phone = Phone.new(account_id: current_account.id,
-                      number: number,
-                      verification_code: session[:verif_code],
-                      submitted_verification_code: phone_params[:code],
-                      validated_at: Time.current)
-
-    if phone.save
+    if current_account.phones.create(number: @phone_number,
+                                     validated_at: Time.current)
       current_account.level_set(:phone)
       redirect_to new_profile_path
     else
-      redirect_to new_phone_url, alert: 'Phone verification failed. Number is invalid or was already verified'
+      redirect_to new_phone_url,
+                  alert: 'Phone verification failed. Number is invalid or was already verified'
     end
   end
 
   def verify
     number = PhoneUtils.sanitize(phone_params[:number])
-    phone = Phone.new(account_id: current_account.id, number: number)
+    phone = current_account.phones.new(number: number)
 
     return render json: { error: 'Phone has already been used' } if phone.number_exists?
 
@@ -49,6 +40,25 @@ class PhonesController < ApplicationController
 
 private
 
+  def check_account_level
+    redirect_to index_path if current_account.level < 1
+  end
+
+  def check_phone
+    @phone_number = PhoneUtils.sanitize(phone_params[:country_code] + phone_params[:number])
+    return if session[:phone] == @phone_number
+
+    redirect_to new_phone_url,
+                alert: 'Confirmation code was sent to another number'
+  end
+
+  def check_code
+    return if PhoneUtils.verify_code(server_code: session[:verif_code],
+                                     user_code: phone_params[:code])
+    redirect_to new_phone_url,
+                alert: 'Verification code is invalid'
+  end
+
   def save_session(phone)
     session[:phone] = phone.number
     session[:verif_code] = phone.code
@@ -58,4 +68,8 @@ private
     params.permit(:country_code, :number, :code)
   end
 
+  def twilio_errors(exception)
+    Rails.logger.error "Twilio Client Error: #{exception.message}"
+    render json: { error: 'Something wrong with Twilio Client' }
+  end
 end
