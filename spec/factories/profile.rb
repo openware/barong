27c1ae83c5b8791123e2209eb3a8fc0d8673017e
Andrey <makovenkoandrey@gmail.com>
2018-04-19 20:08:56 +0300

    Notify user by email when his state changes (#374)

---
+++ spec/factories/profile.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :profile do
+    account { FactoryBot.create(:account) }
+    first_name { Faker::Superhero.name }
+    last_name { Faker::Superhero.name }
+    dob { Faker::Date.birthday }
+    address { Faker::Address.street_address }
+    city { Faker::RickAndMorty.location }
+    country { Faker::Simpsons.location }
+    state { 'validated' }
+  end
+end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,33 +1,41 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+ENV['SENDER_EMAIL'] = 'test@barong.test'
 
 RSpec.describe Profile, type: :model do
 
   ## Test of relationships
-  it { should have_many(:documents).dependent(:destroy) }
   it { should belong_to(:account) }
 
-  context 'Profile with 2 documents' do
-    it do
-      account = Account.create!(email: 'test@gmail.com', password: 'Test123123')
-      profile = Profile.create!(
-         :account => account,
-         :first_name => "MyString",
-         :last_name => "MyString",
-         :address => "MyString",
-         :postcode => "MyString",
-         :city => "MyString",
-         :country => "MyString",
-         :dob => "01-01-2001")
-      document1 = profile.documents.create!(:upload => File.open('app/assets/images/background.jpg'),
-        :doc_type => "MyString",
-        :doc_number => "MyString",
-        :doc_expire => "01-01-2020")
-      document2 = profile.documents.create!(:upload => File.open('app/assets/images/background.jpg'),
-        :doc_type => "MyString",
-        :doc_number => "MyString",
-        :doc_expire => "01-02-2020")
-      expect(profile.reload.documents).to eq([document1, document2])
+  describe 'state update' do
+    let(:profile)  { create :profile }
+    let(:mailer_deliveries) { ActionMailer::Base.deliveries }
+
+    it 'changes account level' do
+      profile.update(state: 'approved')
+      expect(profile.account.level).to eq(3)
+
+      profile.update(state: 'rejected')
+      expect(profile.account.level).to eq(2)
+    end
+
+    after(:each) do
+      mailer_deliveries.clear
+    end
+
+    it 'sends notification emails' do
+      profile.update(state: 'approved')
+      expect(profile.account.level).to eq(3)
+      expect(mailer_deliveries.count).to eq(1)
+      expect(mailer_deliveries.last.to).to eq([profile.account.email])
+      expect(mailer_deliveries.last.subject).to eq('Your identity was approved')
+      expect(mailer_deliveries.last.from).to eq([ENV['SENDER_EMAIL']])
+
+      profile.update(state: 'rejected')
+      expect(profile.account.level).to eq(2)
+      expect(mailer_deliveries.count).to eq(2)
+      expect(mailer_deliveries.last.to).to eq([profile.account.email])
+      expect(mailer_deliveries.last.subject).to eq('Your identity was rejected')
     end
   end
-
 end
