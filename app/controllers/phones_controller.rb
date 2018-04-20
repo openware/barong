e13b4e2e8dbfa37b4b651c58ca7df1fa5fb40955
Dmitry Koval <dkoval@heliostech.fr>
2018-04-20 14:54:44 +0300

    Fix phones controller

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_dependency 'phone_utils'
+
 class PhonesController < ApplicationController
   before_action :authenticate_account!
   before_action :check_account_level
