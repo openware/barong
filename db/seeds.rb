c3b5b93970e5b339e0c220335e0326c6798c3a04
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2018-04-27 22:44:36 +0300

    Use SecureRandom in db:seed (#425)

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ seeds['accounts'].each do |seed|
   end
 
   admin = Account.new(seed['account'])
-  admin.password ||= Faker::Internet.password(15, 20, true)
+  admin.password ||= SecureRandom.hex(20)
 
   if admin.save
     logger.info "Created account for '#{admin.email}'"
