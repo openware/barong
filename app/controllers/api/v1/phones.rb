8ea3e2dd1c7ca225ab2a20d8854e4541a22f2877
Valera Petrusenko <villi1511@gmail.com>
2018-04-27 21:56:44 +0300

    Implement send code API (#423)

--- app/controllers/api/v1/phones.rb
+++ app/controllers/api/v1/phones.rb
@@ -22,6 +22,27 @@ module API
           { message: 'Code was sent successfully' }
         end
 
+        desc 'Resend activation code'
+        params do
+          requires :phone_number, type: String,
+                                  desc: 'Phone number with country code',
+                                  allow_blank: false
+        end
+        post '/send_code' do
+          declared_params = declared(params)
+          return unless phone_valid?(declared_params[:phone_number])
+
+          phone_number = PhoneUtils.sanitize(declared_params[:phone_number])
+          phone = current_account.phones.find_by!(number: phone_number)
+
+          unless phone.regenerate_phone
+            return error!(phone.errors, 422)
+          end
+
+          PhoneUtils.send_confirmation_sms(phone)
+          { message: 'Code was sent successfully' }
+        end
+
         desc 'Verify a phone'
         params do
           requires :phone_number, type: String,
