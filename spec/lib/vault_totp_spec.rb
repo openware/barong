186141eaa05ea58b354f712b744e896c0804476e
Maksim Litvinov <maksim-litvinov@users.noreply.github.com>
2018-07-16 16:21:39 +0300

    Add human errors for cases when vault is down or code was used multiple times

--- spec/lib/vault_totp_spec.rb
+++ spec/lib/vault_totp_spec.rb
@@ -4,6 +4,31 @@ describe Vault::TOTP do
   let(:uid) { 'uid' }
   let(:email) { 'email' }
 
+  describe '.with_human_error' do
+    it 'renders human error when vault is down' do
+      expect do
+        described_class.with_human_error do
+          raise Vault::VaultError, 'Message connection refused message'
+        end
+      end.to raise_error(described_class::Error, '2FA server is under maintenance')
+    end
+
+    it 'renders human error when code was used twice' do
+      expect do
+        described_class.with_human_error do
+          raise Vault::VaultError, 'Message code already used message'
+        end
+      end.to raise_error(described_class::Error,
+        'This code was already used. Wait until the next time period')
+    end
+
+    it 'renders error when called without block' do
+      expect do
+        described_class.with_human_error
+      end.to raise_error(ArgumentError, 'Block is required')
+    end
+  end
+
   describe '.server_available?' do
     subject { described_class.server_available? }
 
