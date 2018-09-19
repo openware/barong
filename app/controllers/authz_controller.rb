738ce5f59ecc28760cf40298726f33f3d9f4b1f6
Maksim Litvinov <1058433+rxx@users.noreply.github.com>
2018-09-19 18:27:43 +0300

    WIP

--- app/controllers/authz_controller.rb
+++ app/controllers/authz_controller.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 class AuthzController < ApplicationController
-  before_action :authenticate_account!, unless: :public_route?
   skip_before_action :verify_authenticity_token
 
   PUBLIC_PATHS = [
-    'diag',
-    'ambassador',
     'accounts/sign_in',
     'accounts/sign_out',
     'accounts/sign_up',
@@ -15,7 +12,11 @@ class AuthzController < ApplicationController
   ]
 
   def verify
+    p current_account
+    p account_signed_in?
+
     return head(:ok) if public_route?
+    return invalid_login_attempt unless authenticate_account!
 
     response.set_header('Authorization', "Bearer #{create_token}")
     head :ok
@@ -24,6 +25,8 @@ class AuthzController < ApplicationController
   private
 
   def create_token
+    # JWT.encode(jwt_payload, Barong::Security.private_key, 'RS256')
+
     Doorkeeper::AccessToken.find_or_create_for(
       Doorkeeper::Application.first,
       current_account.id,
@@ -33,8 +36,29 @@ class AuthzController < ApplicationController
     ).token
   end
 
+  def jwt_payload
+    {
+      iat: Time.current.to_i,
+      exp: 2.minutes.to_i,
+      sub: 'session',
+      iss: 'barong',
+      aud: 'peatio barong',
+      jti: SecureRandom.hex(12).upcase,
+      uid:   current_account.uid,
+      email: current_account.email,
+      role:  current_account.role,
+      level: current_account.level,
+      state: current_account.state
+    }.freeze
+  end
+
   def public_route?
     logger.info "Authz request #{params.inspect}"
     PUBLIC_PATHS.any? { |path| params[:path].include?(path) }
+  end
+
+  def invalid_login_attempt
+    warden.custom_failure!
+    render json: {message: "Require Authentication"}, status: 401
   end
 end
