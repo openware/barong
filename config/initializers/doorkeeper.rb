723b1578de94ba2dd2a9874a6193b3e72fdc4d55
Maksim Litvinov <1058433+rxx@users.noreply.github.com>
2018-09-27 16:22:01 +0300

    Allow to configure scopes from env

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -62,8 +62,11 @@ Doorkeeper.configure do
   # Define access token scopes for your provider
   # For more information go to
   # https://github.com/doorkeeper-gem/doorkeeper/wiki/Using-Scopes
-  # default_scopes  :public
-  optional_scopes :peatio, :barong
+  default_scopes  :peatio, :barong
+
+  if ENV['AUTH_SCOPES'].present?
+    optional_scopes *ENV['AUTH_SCOPES'].split(',').map(&:squish)
+  end
 
   # Change the way client credentials are retrieved from the request object.
   # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
