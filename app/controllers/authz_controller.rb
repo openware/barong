41afc03a5b7f243b7a2e48e2111af7b8698391be
Dima Koval <dkoval@heliostech.fr>
2018-10-31 22:58:56 +0200

    Fix AuthZ controller

--- app/controllers/authz_controller.rb
+++ app/controllers/authz_controller.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
+# Trick or treat (get a JWT for a cookie)
 class AuthzController < ApplicationController
   skip_before_action :verify_authenticity_token
-  # before_action :authenticate_account!, unless: :public_route?
+  before_action :authenticate_account!, unless: :public_route?
 
   PUBLIC_PATHS = [
     'ambassador',
     'accounts/sign_in',
+    'accounts/sign_in/confirm',
     'accounts/sign_out',
     'accounts/sign_up',
     'health/alive',
@@ -14,37 +16,34 @@ class AuthzController < ApplicationController
   ].freeze
 
   def verify
-    return head(:ok) if public_route?
+    unless public_route?
+      jwt_token = JWT.encode(jwt_payload, Barong::Security.private_key, 'RS256')
+      Rails.logger.info { jwt_token }
+      response.set_header('Authorization', "Bearer #{jwt_token}")
+    end
 
-    jwt_token = JWT.encode(jwt_payload, Barong::Security.private_key, 'RS256')
-    response.set_header('Authorization', "Bearer #{jwt_token}")
     head :ok
   end
 
 private
 
-  def current_account
-    Account.first
-  end
-
   def jwt_payload
     {
       iat: Time.current.to_i,
-      exp: 2.minutes.from_now.to_i,
+      exp: 15.seconds.from_now.to_i,
       sub: 'session',
       iss: 'barong',
       aud: 'peatio barong',
       jti: SecureRandom.hex(12).upcase,
-      uid:   current_account.uid,
+      uid: current_account.uid,
       email: current_account.email,
-      role:  current_account.role,
+      role: current_account.role,
       level: current_account.level,
       state: current_account.state
     }.freeze
   end
 
   def public_route?
-    logger.info "Authz request #{params.inspect}"
-    PUBLIC_PATHS.any? { |path| params[:path].to_s.include?(path) }
+    request.format.symbol.in?(%i[css ico js png jpeg]) || PUBLIC_PATHS.include?(params[:path])
   end
 end
