214cb4f86e21731803b2774e768397c870e9ed8f
msmetana <msmetana@heliostech.fr>
2018-11-01 18:54:10 +0200

    Adding Level model

---
+++ app/models/level.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Label level mapping model
+
+class Level < ApplicationRecord
+    validates :key, :value, :description, presence: true
+    validates :value, uniqueness: { scope: :key }
+end
+
+  # == Schema Information
+  # Schema version: 20180907133821
+  #
+  # Table name: levels
+  #
+  #  id          :bigint(8)        not null, primary key
+  #  key         :string(255)
+  #  value       :string(255)
+  #  description :string(255)
+  #  created_at  :datetime         not null
+  #  updated_at  :datetime         not null
+  #
+
