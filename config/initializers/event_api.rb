e7dbf4acbdf065e21bea52a3b0ce51e0dc803507
Valera Petrusenko <villi1511@gmail.com>
2018-11-21 14:21:08 +0200

    Add RabbitMQ middleware for event api

--- config/initializers/event_api.rb
+++ config/initializers/event_api.rb
@@ -1,5 +1,4 @@
-# IMPORTANT: This file is EXPERIMENTAL feature of Barong 1.7.
-# Don't ever try to use it in production.
+# frozen_string_literal: true
 # Specifications are available in docs/specs/event_api.md.
 
 require 'active_support/concern'
@@ -51,11 +50,15 @@ module EventAPI
       end
 
       def notify_record_updated
+        return if record.previous_changes.blank?
+
         current_record  = record
         previous_record = record.dup
         record.previous_changes.each { |attribute, values| previous_record.send("#{attribute}=", values.first) }
 
+        # Guarantee timestamps.
         previous_record.created_at ||= current_record.created_at
+        previous_record.updated_at ||= current_record.created_at
 
         before = previous_record.as_json_for_event_api.compact
         after  = current_record.as_json_for_event_api.compact
@@ -104,6 +107,17 @@ module EventAPI
   # To continue processing by further middlewares return array with event name and payload.
   # To stop processing event return any value which isn't an array.
   module Middlewares
+
+    class << self
+      def application_name
+        Rails.application.class.name.split('::').first.underscore
+      end
+
+      def application_version
+        "#{application_name.camelize}::VERSION".constantize
+      end
+    end
+
     class IncludeEventMetadata
       def call(event_name, event_payload)
         event_payload[:name] = event_name
@@ -113,7 +127,21 @@ module EventAPI
 
     class GenerateJWT
       def call(event_name, event_payload)
-        [event_name, event_payload]
+        jwt_payload = {
+            iss:   Middlewares.application_name,
+            jti:   SecureRandom.uuid,
+            iat:   Time.now.to_i,
+            exp:   Time.now.to_i + 60,
+            event: event_payload
+        }
+
+        private_key = OpenSSL::PKey.read(Base64.urlsafe_decode64(ENV.fetch('EVENT_API_JWT_PRIVATE_KEY')))
+        algorithm   = ENV.fetch('EVENT_API_JWT_ALGORITHM', 'RS256')
+        jwt         = JWT::Multisig.generate_jwt jwt_payload, \
+                                                   { Middlewares.application_name.to_sym => private_key },
+                                                 { Middlewares.application_name.to_sym => algorithm }
+
+        [event_name, jwt]
       end
     end
 
@@ -130,25 +158,49 @@ module EventAPI
       end
     end
 
-    class PublishToAbstractRabbitMQ
+    class PublishToRabbitMQ
+      extend Memoist
+
       def call(event_name, event_payload)
         Rails.logger.debug do
-          ['',
-           'Published new message to RabbitMQ (abstractly):',
-           'exchange    = ' + exchange_name(event_name),
-           'routing key = ' + routing_key(event_name),
-           'payload     = ' + event_payload.to_json,
-           ''
-          ].join("\n")
+          "\nPublishing #{routing_key(event_name)} (routing key) to #{exchange_name(event_name)} (exchange name).\n"
         end
+        exchange = bunny_exchange(exchange_name(event_name))
+        exchange.publish(event_payload.to_json, routing_key: routing_key(event_name))
         [event_name, event_payload]
       end
 
     private
 
-      # TODO: Validate that key include event category.
+      def bunny_session
+        Bunny::Session.new(rabbitmq_credentials).tap do |session|
+          session.start
+          Kernel.at_exit { session.stop }
+        end
+      end
+      memoize :bunny_session
+
+      def bunny_channel
+        bunny_session.channel
+      end
+      memoize :bunny_channel
+
+      def bunny_exchange(name)
+        bunny_channel.direct(name)
+      end
+      memoize :bunny_exchange
+
+      def rabbitmq_credentials
+        return ENV['EVENT_API_RABBITMQ_URL'] if ENV['EVENT_API_RABBITMQ_URL'].present?
+
+        { host:     ENV.fetch('EVENT_API_RABBITMQ_HOST','localhost'),
+          port:     ENV.fetch('EVENT_API_RABBITMQ_PORT','5672'),
+          username: ENV.fetch('EVENT_API_RABBITMQ_USERNAME','guest'),
+          password: ENV.fetch('EVENT_API_RABBITMQ_PASSWORD','guest') }
+      end
+
       def exchange_name(event_name)
-        "barong.events.#{event_name.split('.').first}"
+        "#{Middlewares.application_name}.events.#{event_name.split('.').first}"
       end
 
       def routing_key(event_name)
@@ -160,7 +212,7 @@ module EventAPI
   middlewares << Middlewares::IncludeEventMetadata.new
   middlewares << Middlewares::GenerateJWT.new
   middlewares << Middlewares::PrintToScreen.new
-  middlewares << Middlewares::PublishToAbstractRabbitMQ.new
+  middlewares << Middlewares::PublishToRabbitMQ.new
 end
 
 ActiveSupport.on_load(:active_record) { ActiveRecord::Base.include EventAPI::ActiveRecord::Extension }
