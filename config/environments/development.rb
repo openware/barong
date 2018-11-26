8eb8820c23cba8f63bf912063562eb07865acbfb
alinetskyi <alinetskyi@heliostech.fr>
2018-11-26 17:37:56 +0200

    Set cookie for all subdomains

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  if ENV['ROOT_DOMAIN']
+    config.session_store :cookie_store,
+                         key: '_barong_session',
+                         domain: ".#{ENV.fetch('ROOT_DOMAIN')}"
+  end
 end
