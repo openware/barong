2f7610419a70d8d0993136e1dc794c7f4d5f7f52
MoD <mod@websys.io>
2018-12-10 01:57:21 +0200

    [ci skip] Fixing Drone pipeline with vault

--- config/initializers/vault.rb
+++ config/initializers/vault.rb
@@ -1,10 +1,10 @@
 Barong::App.define do |config|
-  config.set(:vault_adress, 'http://localhost:8200')
-  config.set(:vault_token, 'changeme')
+  config.set(:vault_address, ENV.fetch('BARONG_VAULT_ADDR', 'http://localhost:8200'))
+  config.set(:vault_token, ENV.fetch('BARONG_VAULT_TOKEN', 'changeme'))
 end
 
 Vault.configure do |config|
-  config.address = Barong::App.config.vault_adress
+  config.address = Barong::App.config.vault_address
   config.token = Barong::App.config.vault_token
   config.ssl_verify = false
   config.timeout = 60
