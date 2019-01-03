cc96d5e267745ca2eb59c81ab6dba5dd62c08480
Yehor Chumak <mafara.intro@gmail.com>
2019-01-04 01:19:32 +0200

    Add users entity on login and /me (#706)

---
+++ app/api/v2/entities/user.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module API
+  module V2
+    module Entities
+      # Basic user info
+      class User < Grape::Entity
+        format_with(:iso_timestamp) { |d| d&.utc&.iso8601 }
+
+        expose :email, documentation: { type: 'String' }
+        expose :uid, documentation: { type: 'String' }
+        expose :role, documentation: { type: 'String' }
+        expose :level, documentation: { type: 'Integer' }
+        expose :otp, documentation: { type: 'Boolean', desc: 'is 2FA enabled for account' }
+        expose :state, documentation: { type: 'String' }
+      end
+    end
+  end
+end
