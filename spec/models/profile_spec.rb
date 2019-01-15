7966adf4bbf137909405c42b8b03207fa5f011f2
Yehor Chumak <ychumak@heliostech.fr>
2019-01-15 13:22:05 +0200

    Change regex validations to support non-eng characters

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -24,9 +24,9 @@ RSpec.describe Profile, type: :model do
     ## Test of relationships
     it { should belong_to(:user) }
     it { should validate_presence_of(:first_name) }
-    it { should validate_length_of(:first_name).is_at_least(2).is_at_most(255) }
-    it { should validate_length_of(:city).is_at_least(2).is_at_most(255) }
-    it { should validate_length_of(:last_name).is_at_least(2).is_at_most(255) }
+    it { should validate_length_of(:first_name).is_at_most(255) }
+    it { should validate_length_of(:city).is_at_most(255) }
+    it { should validate_length_of(:last_name).is_at_most(255) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:dob) }
     it { should validate_presence_of(:address) }
