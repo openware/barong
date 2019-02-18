1c2d6668319d8ffecdaed067144344f39377adde
Yehor Chumak <ychumak@heliostech.fr>
2019-02-18 15:31:31 +0200

    Split activity error to wrong topic and no activity

--- app/api/v2/resource/users.rb
+++ app/api/v2/resource/users.rb
@@ -8,6 +8,12 @@ module API::V2
           options[:topic] = 'password'
           record_error!(options)
         end
+
+        def validate_topic!(topic)
+          unless %w[all session otp password].include?(topic)
+            error!({ errors: ['resource.user.wrong_topic'] }, 422)
+          end
+        end
       end
 
       resource :users do
@@ -24,8 +30,10 @@ module API::V2
                               desc: 'Topic of user activity. Allowed: [all, password, session, otp]'
         end
         get '/activity/:topic' do
+          validate_topic!(params[:topic])
           data = current_user.activities
           data = data.where(topic: params[:topic]) if params[:topic] != 'all'
+
           error!({ errors: ['resource.user.no_activity'] }, 422) unless data.present?
 
           data
