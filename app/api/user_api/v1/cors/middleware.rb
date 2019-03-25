b989687ccc1444cb8de3767cb96a99a8432eff9f
Yehor Chumak <mafara.intro@gmail.com>
2019-03-25 18:15:12 +0200

    Allow to configure multiple CORS at barong (#790)

--- app/api/user_api/v1/cors/middleware.rb
+++ app/api/user_api/v1/cors/middleware.rb
@@ -4,8 +4,8 @@ module UserApi
   module V1
     module CORS
       class << self
-        def call(headers)
-          headers.reverse_merge!(self.headers)
+        def call(headers, origin:)
+          headers.reverse_merge!(self.headers(origin))
 
           # Response may differ if server specifies "*" as allowed origins.
           # See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
@@ -16,9 +16,22 @@ module UserApi
           headers
         end
 
-        def headers
+        def headers(origin)
+          whitelisted_origin = ENV.fetch('API_CORS_ORIGINS', '')
+                                  .split(',')
+                                  .map(&:squish)
+                                  .include?(origin)
+          allow_insecure = ENV['API_CORS_ALLOW_INSECURE_ORIGINS'].present?
+          return build_headers(origin) if whitelisted_origin || allow_insecure
+
+          {}
+        end
+
+        private
+
+        def build_headers(origin)
           {
-            'Access-Control-Allow-Origin'      => ENV.fetch('API_CORS_ORIGINS', '*'),
+            'Access-Control-Allow-Origin'      => origin,
             'Access-Control-Allow-Methods'     => 'GET, POST, PUT, PATCH, DELETE',
             'Access-Control-Allow-Headers'     => 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
             'Access-Control-Allow-Credentials' => ENV['API_CORS_ALLOW_CREDENTIALS'].present?.to_s
@@ -29,14 +42,13 @@ module UserApi
       class Middleware < Grape::Middleware::Base
         def call(env)
           request = Grape::Request.new(env)
-          if request.options?
-            [200, CORS.headers, []]
-          else
-            response = @app.call(env)
-            headers = response.is_a?(Array) ? response[1] : response.headers
-            CORS.call(headers)
-            response
-          end
+          origin = request.headers['Origin']
+          return [200, CORS.headers(origin), []] if request.options?
+
+          response = @app.call(env)
+          headers = response.is_a?(Array) ? response[1] : response.headers
+          CORS.call(headers, origin: origin)
+          response
         end
       end
     end
