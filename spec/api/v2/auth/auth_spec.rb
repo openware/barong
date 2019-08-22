9dc4ff68ce898ae27dd9bd54d43835cedcd603f6
Denis <dfedorchenko@heliostech.fr>
2019-08-22 17:17:53 +0300

    Add session autorenew

--- spec/api/v2/auth/auth_spec.rb
+++ spec/api/v2/auth/auth_spec.rb
@@ -39,10 +39,16 @@ describe '/api/v2/auth functionality test' do
         expect(response.headers['Authorization']).to include "Bearer"
       end
 
-      it "doesn't return set-cookie header on valid session" do
-        get auth_request
+      it 'returns set-cookie header with updated expiration' do
+        expect {
+          get auth_request
+        }.to change { response.cookies }
+
         expect(response.status).to eq(200)
-        expect(response.headers['Set-Cookie']).to be_nil
+        expect(response.headers['Set-Cookie']).not_to be_nil
+        expect(
+          Time.parse(response.headers['Set-Cookie'].split(';')[-2].split('=')[1])
+        ).to be_within(10).of(Time.now + 30.minutes)
       end
 
       it 'allows any type of request' do
@@ -63,12 +69,6 @@ describe '/api/v2/auth functionality test' do
         get auth_request
         expect(response.status).to eq(401)
         expect(response.body).to eq("{\"errors\":[\"authz.invalid_session\"]}")
-      end
-
-      it "doesn't return set-cookie header on invalid session" do
-        get auth_request
-        expect(response.status).to eq(401)
-        expect(response.headers['Set-Cookie']).to be_nil
       end
 
       it 'renders error if session belongs to non-active user' do
