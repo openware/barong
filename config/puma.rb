9b4cd625e3f7dd4384bcd929c4c7fb56225795cf
alinetskyi <artem.linetskiy@gmail.com>
2020-01-16 14:38:51 +0200

    Feature: Make processes and threads configurable

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,11 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { 1 }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 6 }
+
+threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -21,7 +24,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 1 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
