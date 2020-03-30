b246d0a00aeffabc7148bb01f2366395d52bb2a9
Chumak Nadiia <chumaknadya19@gmail.com>
2020-03-30 15:03:10 +0300

    Enhancement: Add created_at and updated_at to profile entities

--- app/api/v2/entities/profile.rb
+++ app/api/v2/entities/profile.rb
@@ -12,6 +12,11 @@ module API::V2
       expose :country, documentation: { type: 'String' }
       expose :state, documentation: { type: 'String' }
       expose :metadata, documentation: { type: 'Hash', desc: 'Profile additional fields' }
+
+      with_options(format_with: :iso_timestamp) do
+        expose :created_at
+        expose :updated_at
+      end
     end
   end
 end
