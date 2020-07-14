bad075f66f3b7f395ee8fc8fa7a7c007835fa1d7
Maksym Naichuk <mnaichuk@heliostech.fr>
2020-07-14 10:58:02 +0300

    Feature: Add redis cluster support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,11 @@ Rails.application.configure do
 
   # Using cache for sessions and permissions forces to use redis cache_store as mandatory store
   # Here we use ENV.fetch instead of Barong::App.config, because environment/* files loads before lib and initializers
-  config.cache_store = :redis_cache_store, { driver: :hiredis, url: ENV.fetch('BARONG_REDIS_URL', 'redis://localhost:6379/1') }
+  if ENV.true?('BARONG_REDIS_CLUSTER')
+    config.cache_store = :redis_cache_store, { driver: :hiredis, cluster: [ENV.fetch('BARONG_REDIS_URL')], password: ENV.fetch('BARONG_REDIS_PASSWORD') }
+  else
+    config.cache_store = :redis_cache_store, { driver: :hiredis, url: ENV.fetch('BARONG_REDIS_URL', 'redis://localhost:6379/1') }
+  end
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
