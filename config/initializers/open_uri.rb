fb2a7b10c16614352648338f17d9a92126a1fe0a
Yehor Chumak <mafara.intro@gmail.com>
2020-08-14 16:12:40 +0300

    Handle OpenURI::HTTPError 404 && avoid OpenURI StringIO creation (#1132)
    * decrease OpenURI::Buffer::StringMax to 0 to force Temfile creation
    * rescue OpenURI::HTTPError in KYCAID workers

---
+++ config/initializers/open_uri.rb
@@ -0,0 +1,4 @@
+# Don't allow downloaded files to be created as StringIO. Create a tempfile instead
+
+OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
+OpenURI::Buffer.const_set 'StringMax', 0
