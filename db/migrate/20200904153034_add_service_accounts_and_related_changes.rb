12776b84e30a641fff1a79ed47ee07b078d01e33
Danylo Patsora <dpatsora@heliostech.fr>
2020-09-04 19:06:09 +0300

    Make migration reversible

--- db/migrate/20200904153034_add_service_accounts_and_related_changes.rb
+++ db/migrate/20200904153034_add_service_accounts_and_related_changes.rb
@@ -3,7 +3,7 @@ class AddServiceAccountsAndRelatedChanges < ActiveRecord::Migration[5.2]
     self.table_name = :apikeys
   end
 
-  def change
+  def up
     create_table :service_accounts do |t|
       t.string    :uid,                 null: false
       t.bigint    :owner_id,            null: false, unsigned: true
@@ -25,6 +25,26 @@ class AddServiceAccountsAndRelatedChanges < ActiveRecord::Migration[5.2]
     end
 
     remove_column :apikeys, :user_id
-    add_index :apikeys, [:key_holder_account_type, :key_holder_account_id], name: :idx_apikey_on_account
+    add_index :apikeys, [:key_holder_account_type, :key_holder_account_id], name: :idx_apikey_on_account unless index_exists?(:service_accounts, [:key_holder_account_type, :key_holder_account_id])
+  end
+
+  def down
+    add_column :apikeys, :user_id, :bigint, unsigned: true, null: false, after: :key_holder_account_id
+
+    APIKey.find_each do |api_key|
+      if api_key.key_holder_account_type == 'ServiceAccount'
+        api_key.destroy!
+        next
+      end
+
+      api_key.user_id = api_key.key_holder_account_id
+      api_key.save!
+    end
+
+    remove_column :apikeys, :key_holder_account_id
+    remove_column :apikeys, :key_holder_account_type
+
+    remove_index :service_accounts, column: [:key_holder_account_type, :key_holder_account_id] if index_exists?(:service_accounts, [:key_holder_account_type, :key_holder_account_id])
+    drop_table :service_accounts
   end
 end
