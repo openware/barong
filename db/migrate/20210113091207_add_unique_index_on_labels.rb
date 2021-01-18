0fd907e31edbae6239a55f6cfb9e22d47fafe8c2
Camille <cmeulien@heliostech.fr>
2021-01-18 17:35:57 +0200

    Update 20210113091207_add_unique_index_on_labels.rb

--- db/migrate/20210113091207_add_unique_index_on_labels.rb
+++ db/migrate/20210113091207_add_unique_index_on_labels.rb
@@ -2,7 +2,7 @@ class AddUniqueIndexOnLabels < ActiveRecord::Migration[5.2]
   def change
     remove_index :labels, [:user_id, :key, :scope]
 
-    # Delete dublicated values
+    # Delete duplicated values
     ids = ActiveRecord::Base.connection.execute("SELECT MAX(ID) from labels l GROUP BY l.user_id, l.scope, l.key")
 
     Label.where.not(id: ids.to_a.flatten).find_each do |label|
