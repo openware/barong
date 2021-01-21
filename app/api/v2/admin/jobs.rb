733a8e23be253fd44f5d18de9fc1694b6980e31f
Keng Suppaseth <kengio@gmail.com>
2021-01-21 16:06:47 +0700

    Add Admin job API

--- app/api/v2/admin/jobs.rb
+++ app/api/v2/admin/jobs.rb
@@ -6,14 +6,58 @@ module API
       # Admin functionality over jobs table
       class Jobs < Grape::API
         resource :jobs do
-          # POST request
-          # description - required
-          # start_at - required
-          # finish_at (optional)
-          # type - required (default maintenance)
-          # whitelist_ip (optional)
-          # Create Job with reference to Restriction +
-          # Restriction to whitelist if whitelist_ip present => create Restriction for whitelist specific IP
+          helpers ::API::V2::NamedParams
+
+          desc 'Create new job',
+            failure: [
+              { code: 401, message: 'Invalid bearer token' }
+            ],
+            success: { code: 200, message: 'Job was created' }
+          params do
+            requires :description,
+                     type: String,
+                     allow_blank: false,
+                     desc: 'job description'
+            requires :job_type,
+                     type: String,
+                     values: { value: -> { ['maintenance'] }, message: 'admin.job.invalid_job_type'},
+                     desc: 'job type'
+            requires :start_at,
+                     type: DateTime,
+                     desc: 'time to run start job'
+            optional :finish_at,
+                     type: DateTime,
+                     desc: 'time to run finish job'
+            optional :whitelist_ip,
+                     type: String,
+                     desc: 'whitelist IP address'
+          end
+          post do
+            admin_authorize! :create, Job
+            admin_authorize! :create, Restriction
+            
+            # Create or find maintenace restriction
+            restriction = Restriction.find_or_create_by(category: params[:job_type], scope: 'all', value: 'all', state: 'disabled')
+
+            # Set parameters
+            declared_params = declared(params, include_missing: false)
+            job_params = declared_params.merge(reference: restriction)
+                                        .except(:whitelist_ip)
+
+            # Create new job
+            job = Job.new(job_params)
+            code_error!(job.errors.details, 422) unless job.save
+
+            # Create new whitelist restriction for specific IP address if present
+            if params[:whitelist_ip].present?
+              Restriction.create!(scope: 'ip', category: 'whitelist', value: params[:whitelist_ip])
+              
+              # clear cached restrictions, so they will be freshly refetched on the next call to /auth
+              Rails.cache.delete('restrictions')
+            end
+
+            status 200
+          end
 
           # GET request
           # List for existing Jobs with pagination
