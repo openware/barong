aa8cdefe12cafc4eecdc4ad63b95b0cefe303e51
Keng Suppaseth <kengio@gmail.com>
2021-01-22 14:18:23 +0700

    Fix comment in job migration

--- db/migrate/20210120100145_create_jobs.rb
+++ db/migrate/20210120100145_create_jobs.rb
@@ -1,7 +1,7 @@
 class CreateJobs < ActiveRecord::Migration[5.2]
   def change
     create_table :jobs do |t|
-      t.integer    :type,        null: false # enum ( whitelist: 0, maintenance: 1, blacklist: 2, blocklogin: 3 )
+      t.integer    :type,        null: false # enum ( maintenance: 0 )
       t.text       :description, null: false, limit: 5120
       t.integer    :state,       null: false # enum ( pending: 0, active: 1, disabled: 2 )
       t.datetime   :start_at,    null: false
