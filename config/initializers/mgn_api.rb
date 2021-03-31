0123a219a25f6db3b0fb870b40f33583d4346226
Camille <cmeulien@heliostech.fr>
2021-03-31 17:57:32 +0300

    Feature: Add the support for ERB in management_api.yml (#1267)

--- config/initializers/mgn_api.rb
+++ config/initializers/mgn_api.rb
@@ -1,11 +1,19 @@
 require 'yaml'
+require 'erb'
 require 'openssl'
 
-(YAML.load_file('config/management_api.yml') || {}).deep_symbolize_keys!.tap do |x|
+raw_file = File.read('config/management_api.yml')
+yaml = ERB.new(raw_file).result
+
+(::YAML.safe_load(yaml) || {}).deep_symbolize_keys!.tap do |x|
   x.fetch(:keychain).each do |id, key|
-    key = OpenSSL::PKey.read(Base64.urlsafe_decode64(key.fetch(:value)))
+    begin
+      key = OpenSSL::PKey.read(Base64.urlsafe_decode64(key.fetch(:value)))
+    rescue OpenSSL::PKey::PKeyError
+      raise "Invalid public key format for key #{id} (value: #{key.fetch(:value)})"
+    end
     if key.private?
-      raise ArgumentError, 'keychain.' + id.to_s + ' was set to private key, ' \
+      raise ArgumentError, "keychain. #{id} was set to private key, " \
         'however it should be public (in config/management_api.yml).'
     end
     x[:keychain][id][:value] = key
@@ -14,9 +22,8 @@ require 'openssl'
   x.fetch(:scopes).values.each do |scope|
     %i[permitted_signers mandatory_signers].each do |list|
       scope[list] = scope.fetch(list, []).map(&:to_sym)
-      scope[list] = scope.fetch(list, []).map(&:to_sym)
       if list == :mandatory_signers && scope[list].empty?
-        raise ArgumentError, 'scopes.' + scope.to_s + '.' + list.to_s + ' is empty, ' \
+        raise ArgumentError, "scopes.#{scope}.#{list} is empty, " \
           'however it should contain at least one value (in config/management_api.yml).'
       end
     end
