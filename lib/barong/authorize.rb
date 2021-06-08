f55b5d353bbe4e97a661f735ade03d65a1ce104d
Danil Pismenny <danil@brandymint.ru>
2021-06-08 14:42:12 +0300

    Improve barong/authorize

--- lib/barong/authorize.rb
+++ lib/barong/authorize.rb
@@ -117,13 +117,10 @@ module Barong
 
     def validate_bitzlato_user!(user)
       bitzlato_user = BitzlatoUser.find_by_email(user.email)
-      if bitzlato_user.present?
-        if bitzlato_user.user_profile.try(&:blocked_by_admin?)
-          Rails.logger.warn("Bitzlato user #{bitzlato_user.real_email} is blocked by admin")
-          error!({ errors: ['authz.blocked_account'] }, 401)
-        end
-      else
-        Rails.logger.info("Not Found Bitzlato user with email #{user.email}")
+      return if bitzlato_user.nil?
+      if bitzlato_user.user_profile.try(&:blocked_by_admin?)
+        Rails.logger.warn("Bitzlato user #{bitzlato_user.real_email} is blocked by admin")
+        error!({ errors: ['authz.blocked_account'] }, 401)
       end
     end
 
