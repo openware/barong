0c4b48ae57ba6d78bc27d0d39dceaafc6a7f704b
Danil Pismenny <danil@brandymint.ru>
2021-06-17 15:56:32 +0300

    Enable bitzlato user feature market validation

--- lib/barong/authorize.rb
+++ lib/barong/authorize.rb
@@ -118,7 +118,8 @@ module Barong
     def validate_bitzlato_user!(user)
       return unless ENV.true? 'USE_BITZLATO_AUTHORIZATION'
       bitzlato_user = BitzlatoUser.find_by_email(user.email)
-      return if bitzlato_user.nil?
+      error!({ errors: ['authz.unknown_bitzlato_account'] }, 401) if bitzlato_user.nil?
+      error!({ errors: ['authz.no_user_feature_market'] }, 401) if bitzlato_user.user_features.where(feature_code: 'MARKET').empty?
       if bitzlato_user.user_profile.try(&:blocked_by_admin?)
         Rails.logger.warn("Bitzlato user #{bitzlato_user.real_email} is blocked by admin")
         error!({ errors: ['authz.blocked_account'] }, 401)
