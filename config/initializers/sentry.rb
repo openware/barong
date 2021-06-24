d44205c437b5f5155da258f80f40d1ffbc565ccc
Danil Pismenny <danil@brandymint.ru>
2021-06-24 10:48:37 +0300

    Update sentry performance settings

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,16 +2,7 @@
 
 Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-
-  # Set tracesSampleRate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production
-  config.traces_sample_rate = 1
-  # or
-  config.traces_sampler = lambda do |_context|
-    true
-  end
-
+  config.traces_sample_rate = 0.1
   config.send_default_pii = true
   config.enabled_environments = %w[production staging]
 end
