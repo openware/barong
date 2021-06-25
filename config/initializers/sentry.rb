5772065824dcccd0eafa3885aaf0724ebd0ece2c
Danil Pismenny <danil@brandymint.ru>
2021-06-25 08:13:21 +0300

    Decrease Sentry traces sample rate to 0.01

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@
 
 Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.traces_sample_rate = 0.1
+  config.traces_sample_rate = 0.01
   config.send_default_pii = true
   config.enabled_environments = %w[production staging]
 end
