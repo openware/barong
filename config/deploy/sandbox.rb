b32954890f371bc3c8df83d1d796d69e821e9b46
mailer21 <metelew@gmail.com>
2021-09-12 13:12:21 +0300

    market-sandbox added to capistrano

---
+++ config/deploy/sandbox.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+set :rails_env, :staging
+
+set :application, -> { 'barong-sandbox' }
+set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
+
+server '217.182.138.99',
+  user: fetch(:user),
+  port: '22',
+  roles: %w[app db].freeze,
+  ssh_options: { forward_agent: true }
