05e982b31001106c7d3b5f3f1b7c25255a0e38a5
Eugene Stepanov <antynitr@gmail.com>
2021-11-22 13:58:32 +0300

    Fix. Tests

--- spec/lib/barong/authorize_spec.rb
+++ spec/lib/barong/authorize_spec.rb
@@ -10,53 +10,50 @@ describe Barong::Authorize do
              verb: 'all'
     end
 
+    let(:secret) { ENV.fetch('P2P_SESSION_SECRET') }
+    let(:session_id) { 'session_id' }
     let(:request) do
-      double(
-        headers: {},
-        cookies: { 'sessionCookie' => 'sess:xxx.xxxxxx'},
-        remote_ip: '85.140.0.148',
-        env: { 'HTTP_USER_AGENT': 'blabla'}
-      )
+      request = ActionDispatch::TestRequest.create
+      request.cookies['sessionCookie'] = Barong::BitzlatoSession.generate_cookie(session_id, secret)
+      request.session = ActionController::TestSession.new
+      request.session.id = session_id
+      request
     end
-
-    subject { described_class.new(request, "") }
 
     def auth!(jwt_payload = {})
-      allow_any_instance_of(Barong::BitzlatoSession).to receive(:id_token)
+      session_data = {}
+      session_data[:passport] = { user: { userId: 1, idToken: jwt_payload.to_json } }
+
+      allow_any_instance_of(Redis).to receive(:get) { session_data.to_json }
       allow(Barong::Auth0::JWT).to receive(:verify).and_return([jwt_payload.with_indifferent_access])
 
-      subject.bz_cookie_owner
+      described_class.new(request, "").bz_cookie_owner
     end
-
-    #it 'call bz_cookie_owner' do
-    #  expect(subject).to receive(:bz_cookie_owner)
-    #  auth!
-    #end
 
     context 'when user exists' do
       let(:user) { create :user }
-      let(:payload) { {email: user.email, email_verified: true} }
+      let(:jwt_payload) { { email: user.email, email_verified: true } }
 
-      it { expect(auth!(payload)).to eq(user) }
+      it { expect(auth!(jwt_payload)).to eq(user) }
     end
 
     context 'when user not exists' do
-      let(:payload) { {email: 'email@example.test', email_verified: true} }
+      let(:jwt_payload) { { email: 'email@example.test', email_verified: true } }
 
-      it { expect { auth!(payload) }.to change { User.count}.by(1) }
-      it { expect { auth!(payload) }.to change { Label.count}.by(1) }
+      it { expect { auth!(jwt_payload) }.to change { User.count}.by(1) }
+      it { expect { auth!(jwt_payload) }.to change { Label.count}.by(1) }
     end
 
     context 'when email not verified' do
-      let(:payload) { {email: 'email@example.test', email_verified: false} }
+      let(:jwt_payload) { { email: 'email@example.test', email_verified: false } }
 
-      it { expect { auth!(payload) }.to raise_error(Barong::Authorize::AuthError) }
+      it { expect { auth!(jwt_payload) }.to raise_error(Barong::Authorize::AuthError) }
     end
 
     context 'when no email key' do
-      let(:payload) { { email_verified: true } }
+      let(:jwt_payload) { { email_verified: true } }
 
-      it { expect { auth!(payload) }.to raise_error(Barong::Authorize::AuthError) }
+      it { expect { auth!(jwt_payload) }.to raise_error(Barong::Authorize::AuthError) }
     end
   end
 end
