d4230a699cf0dd66986e8ff53e2385ed9fb3d267
mailer21 <metelew@gmail.com>
2021-11-29 23:55:23 +0300

    stages moving to server with internal ip

--- config/deploy/s2.rb
+++ config/deploy/s2.rb
@@ -1,12 +1,1 @@
-# frozen_string_literal: true
-
-set :rails_env, :staging
-set :disallow_pushing, false
-
-set :application, -> { 'barong-' + fetch(:stage).to_s }
-
-server '87.98.150.101',
-  user: fetch(:user),
-  port: '22',
-  roles: %w[app db].freeze,
-  ssh_options: { forward_agent: true }
+s1.rb
