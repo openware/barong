12353b39335fe95734430546b8028ca6bf708c13
chumaknadya <chumaknadya19@gmail.com>
2021-12-08 15:26:29 +0200

    Enhancement: Add rate limitation && add secure flag for session (#1346)

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 # Use cache_store as session_store for Rails sessions. Key default is '_barong_session'
-Rails.application.config.session_store :cache_store, key: Barong::App.config.session_name, expire_after: 24.hours.seconds
+
+Rails.application.config.session_store :cache_store, key: Barong::App.config.session_name,
+                                                     expire_after: 24.hours.seconds,
+																										 secure: Barong::App.config.tls_enabled && Rails.env.production?
