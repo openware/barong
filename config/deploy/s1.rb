37e52b9a711af77360d437f9978bac2a60b8f2c4
Aleksandr B. Ivanov <radanisk@users.noreply.github.com>
2021-12-12 08:53:51 +0300

    Update withdraw mails (#51)
    Resolves: https://github.com/bitzlato/peatio/issues/306

--- config/deploy/s1.rb
+++ config/deploy/s1.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+set :user, 'app'
+set :systemd_mailer_role, :app
+
 set :rails_env, :staging
 set :disallow_pushing, false
 set :application, -> { 'barong-' + fetch(:stage).to_s }
