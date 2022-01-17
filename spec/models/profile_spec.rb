c1eae9442d4f208a4cfec81ecd9925dac68a52a5
Danil Pismenny <danil@brandymint.ru>
2022-01-17 09:54:52 +0300

    Update db/schema.rb

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -62,6 +62,12 @@ RSpec.describe Profile, type: :model do
 
         before do
           subject.update(params)
+          puts
+          puts '---'
+          puts Profile.new.as_json
+          puts subject.id
+          puts Profile.pluck(:user_id, :id, :state).join(':')
+          puts '---'
         end
 
         it { expect(subject).to be_valid }
@@ -73,7 +79,8 @@ RSpec.describe Profile, type: :model do
         it { expect(subject.city.present?).to be_truthy }
         it { expect(subject.country.present?).to be_truthy }
         it { expect(subject.metadata.nil?).to be_truthy }
-        it { expect(subject.state).to eq('drafted') }
+        fit { expect(subject).to be_persisted }
+        fit { expect(subject.state).to eq('drafted') }
         it { expect(subject.user.labels.find_by(key: 'profile').value).to eq('drafted') }
       end
     end
