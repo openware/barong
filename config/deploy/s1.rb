0f1d9eb6b1c8e06467373fb551475ee72ae7fa94
Danil Pismenny <danil@brandymint.ru>
2022-01-19 14:01:15 +0300

    Add mutiple puma bind for s1

--- config/deploy/s1.rb
+++ config/deploy/s1.rb
@@ -5,8 +5,8 @@ set :user, 'app'
 set :rails_env, :staging
 set :disallow_pushing, false
 set :application, -> { 'barong-' + fetch(:stage).to_s }
-
 set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
+set :puma_bind, -> { ["tcp://0.0.0.0:960#{fetch(:stage)[1].to_i || 9}", "unix://#{shared_path}/tmp/sockets/puma.sock"] }
 
 server ENV.fetch('STAGING_SERVER'),
   user: fetch(:user),
